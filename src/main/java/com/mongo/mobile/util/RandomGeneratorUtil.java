5e414548791d10331f312a4650e93ee7a5db8b59
hes <heshengyx@qq.com>
2014-10-31 11:24:03 +0800

    add

---
+++ src/main/java/com/mongo/mobile/util/RandomGeneratorUtil.java
@@ -0,0 +1,18 @@
+package com.mongo.mobile.util;
+
+import java.util.Random;
+
+public class RandomGeneratorUtil {
+
+	private static String range = "ACBDEFGHJKLMNPQRSTUVWXYZ123456789";
+	
+	public static synchronized String getRandomString(int length) {
+		Random random = new Random();
+		StringBuffer result = new StringBuffer();
+		
+		for (int i = 0; i < length; i++) {
+			result.append(range.charAt(random.nextInt(range.length())));
+		}
+		return result.toString();
+	}
+}
---
+++ src/main/java/com/mongo/mobile/util/StringUtil.java
@@ -0,0 +1,12 @@
+package com.mongo.mobile.util;
+
+public class StringUtil {
+
+	public static String getMethodName(String field) {
+        return "get" + field.substring(0, 1).toUpperCase() + field.substring(1);
+    }
+	
+	public static String setMethodName(String field) {
+        return "set" + field.substring(0, 1).toUpperCase() + field.substring(1);
+    }
+}
